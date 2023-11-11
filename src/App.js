e76bf27a5854ad2f7b7456aae24df3d7febc8187
Makarios-Tech101 <ogodogublessing1@gmail.com>
2023-11-11 21:23:32 +0100

    "date_counter_app styled"

--- src/App.js
+++ src/App.js
@@ -20,20 +20,31 @@ function Counter() {
   date.setDate(date.getDate() + count);
 
   return <div className="container">
-    <div>
-      <button onClick={() => setStep((c) => c-1)}> - </button> Step : {step}<button onClick={()=> setStep ((c) => c+1)}> + </button>
+    <div className="box">
+        <div className='button-one'>
+          <button id="btn-one" onClick={() => setStep((c) => c-1)}> Reduce step(s) </button> 
+            <span>
+               Step : {step}
+            </span>
+         <button id="btn-two"  onClick={()=> setStep ((c) => c+1)}> Add steps </button>
+        </div>
+
+        <div className='button-two'>
+          <button id="btn-three"  onClick={() => setCount((c) => c-step)}> Reduce count(s)</button> 
+            <span> 
+              Count : {count} 
+            </span>
+          <button id="btn-one"  onClick={()=> setCount ((c) => c+step)}> Add count(s) </button> 
+        </div>
+
+        <p>
+          <span>
+            {count === 0 ? "Today is" : count > 0 ? `${count} days from today will be ` : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>
+            {date.toDateString()}
+          </span>
+        </p>
     </div>
-
-    <div>
-      <button onClick={() => setCount((c) => c-step)}> - </button> Count :{count} <button onClick={()=> setCount ((c) => c+step)}> + </button> 
-    </div>
-
-    <p>
-      <span>
-        {count === 0 ? "Today is" : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `}
-      </span>
-      {date.toDateString()}
-    </p>
-
   </div> 
 }
